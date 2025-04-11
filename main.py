c0c7a004a32b1087c8931cc5a41e565685de8d14
ivank <vk53418006@gmail.com>
2025-04-11 10:25:03 +0300

    Remake logic

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI, BackgroundTasks
+import os.path
+from http.client import HTTPException
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from services import FilesHandler
+
+from fastapi import FastAPI, BackgroundTasks, UploadFile, HTTPException, WebSocketException
 import services
-from services.pdf_text_parser import get_pdf_text
+from models.QuestionData import QuestionData
+from services.FilesHandler import process_files_to_vector_db, ask_question
+import aiofiles
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,9 +28,21 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
 @app.get("/updateBase", status_code=200)
-async def update(background_tasks: BackgroundTasks):
-    background_tasks.add_task(get_pdf_text, "Docs\\bazi_dannih.pdf")
-    #return {"message": await get_pdf_text("Docs\\bazi_dannih.pdf")}
+async def updateVectorBase(background_tasks: BackgroundTasks):
+    background_tasks.add_task(process_files_to_vector_db)
     return {"message": "Обработка запущена"}
+
+@app.post("/uploadfile/")
+async def upload_file(files: list[UploadFile]):
+    files_exist_list = []
+    for file in files:
+        async with aiofiles.open(f'Docs\\{file.filename}', "wb") as out_file:
+            content = await file.read()
+            await out_file.write(content)
+    return {"filename": [file.filename for file in files], "Result": "OK"}
+
+
+@app.post("/getAnswer/")
+async def getAnswer(request: QuestionData):
+    return {"question": await ask_question(request.question)}
